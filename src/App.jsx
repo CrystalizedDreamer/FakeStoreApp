2e824e72239b9fc33cdc3d96d8fe0cdce55c3890
William <ware.william.joe@gmail.com>
2025-06-15 21:47:09 -0400

    Correction for the conventional handling of components

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import  { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import { useEffect } from 'react';
 import React from 'react';     
-import Products from './assets/products.jsx'; 
-import AddProducts  from './assets/AddProducts.jsx';
-
-function Home() {
-  return (
-    <div className="container mt-5">
-      <div id='HomePlaceholder'>
-        <h1 className='text-center'>FakeStore App</h1>
-        <h2 className='TempHeading'>Welcome Home</h2>
-        <p>This is a home page!</p>
-        <p>...Or at least it would be if this was a real webpage</p>
-        <br />
-        <p>Click on the links above to navigate to the Products or Add Products pages.</p>
-        <p><i>Note: This is a fake store app, so the products are not real. Additionally, there is no add to cart feature.</i></p>
-      </div>
-      
-    </div>
-  )
-}
-
+import Products from './components/products.jsx'; 
+import AddProducts  from './components/addProducts.jsx';
+import Home from './components/home.jsx';
 
 
 function App() {
